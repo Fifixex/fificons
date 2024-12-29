b207050a6fedd109ede86fb7d90443bb28d1a19f
Fifixex <122852085+Fifixex@users.noreply.github.com>
2024-12-29 14:26:35 +0000

    :art: style: formating code

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,10 +3,10 @@ import { readdirSync, readFileSync, writeFileSync } from 'node:fs'
 import { optimizeSVGO } from './optimize'
 
 const directory = resolve(__dirname, '..', 'icons')
-const icons = readdirSync(directory).filter((file) => file.endsWith('.svg'));
+const icons = readdirSync(directory).filter((file) => file.endsWith('.svg'))
 
 for (const file of icons) {
-    const filePath = resolve(directory, file);
-    const buffer = readFileSync(filePath);
-    writeFileSync(filePath, await optimizeSVGO(filePath, buffer))
+	const filePath = resolve(directory, file)
+	const buffer = readFileSync(filePath)
+	writeFileSync(filePath, await optimizeSVGO(filePath, buffer))
 }
--- scripts/options.ts
+++ scripts/options.ts
@@ -1,27 +1,27 @@
 import type { Config as SVGOConfig } from 'svgo'
 export const OPTIONS = {
-    multipass: true,
-    plugins: [
-      {
-        name: 'preset-default',
-        params: {
-          cleanupIDs: {
-            minify: false,
-            remove: false,
-          },
-          convertPathData: false,
-          overrides: {
-            cleanupNumericValues: false,
-            removeViewBox: false, // https://github.com/svg/svgo/issues/1128
-          },
-        },
-      },
-      'sortAttrs',
-      {
-        name: 'addAttributesToSVGElement',
-        params: {
-          attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
-        },
-      },
-    ],
+	multipass: true,
+	plugins: [
+		{
+			name: 'preset-default',
+			params: {
+				cleanupIDs: {
+					minify: false,
+					remove: false
+				},
+				convertPathData: false,
+				overrides: {
+					cleanupNumericValues: false,
+					removeViewBox: false
+				}
+			}
+		},
+		'sortAttrs',
+		{
+			name: 'addAttributesToSVGElement',
+			params: {
+				attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }]
+			}
+		}
+	]
 } as SVGOConfig
